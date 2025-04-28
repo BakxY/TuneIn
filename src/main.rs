f3d87cbf98bfaeb7f366ce6bfadc650c7be7bff0
Severin Sprenger <severin.sprenger@sespra.ch>
2025-04-28 21:14:18 +0200

    Added basic tui layout, code will need to be cleaned up

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,122 @@
-fn main() {
-    println!("Hello, world!");
+use crossterm::event::{self, Event};
+use ratatui::{
+    DefaultTerminal, Frame,
+    layout::{Constraint, Direction, Layout, Rect},
+    style::{Color, Style, Stylize},
+    symbols,
+    widgets::{Block, BorderType, Borders, LineGauge},
+};
+use std::{io::Result, rc::Rc};
+
+fn main() -> Result<()> {
+    let terminal = ratatui::init();
+    let result = run(terminal);
+    ratatui::restore();
+    result
+}
+
+fn run(mut terminal: DefaultTerminal) -> Result<()> {
+    loop {
+        terminal.draw(render)?;
+        if matches!(event::read()?, Event::Key(_)) {
+            break Ok(());
+        }
+    }
+}
+
+fn generate_layout(frame: &mut Frame) -> (Rc<[Rect]>, Vec<Rect>) {
+    let vertical_temp_layout = Layout::default()
+        .direction(Direction::Horizontal)
+        .constraints(vec![Constraint::Percentage(20), Constraint::Percentage(80)])
+        .split(frame.area());
+
+    let general_layout = Layout::default()
+        .direction(Direction::Vertical)
+        .constraints(vec![
+            Constraint::Percentage(33),
+            Constraint::Percentage(33),
+            Constraint::Percentage(33),
+        ])
+        .split(vertical_temp_layout[0]);
+
+    let midi_temp_layout = Layout::default()
+        .direction(Direction::Vertical)
+        .constraints(vec![Constraint::Percentage(50), Constraint::Percentage(50)])
+        .split(vertical_temp_layout[1]);
+
+    let mut midi_layout: Vec<Rect> = Vec::new();
+
+    midi_layout.append(
+        &mut Layout::default()
+            .direction(Direction::Horizontal)
+            .constraints(vec![
+                Constraint::Percentage(20),
+                Constraint::Percentage(20),
+                Constraint::Percentage(20),
+                Constraint::Percentage(20),
+                Constraint::Percentage(20),
+            ])
+            .split(midi_temp_layout[0])
+            .to_vec(),
+    );
+
+    midi_layout.append(
+        &mut Layout::default()
+            .direction(Direction::Horizontal)
+            .constraints(vec![
+                Constraint::Percentage(20),
+                Constraint::Percentage(20),
+                Constraint::Percentage(20),
+                Constraint::Percentage(20),
+                Constraint::Percentage(20),
+            ])
+            .split(midi_temp_layout[1])
+            .to_vec(),
+    );
+
+    return (general_layout, midi_layout);
+}
+
+fn render(frame: &mut Frame) {
+    let (general_layout, midi_layout) = generate_layout(frame);
+
+    frame.render_widget(
+        Block::new()
+            .border_type(BorderType::Rounded)
+            .borders(Borders::ALL)
+            .border_style(Style::default().fg(Color::White))
+            .style(Style::default())
+            .title("Info"),
+        general_layout[0],
+    );
+    frame.render_widget(
+        Block::new()
+            .border_type(BorderType::Rounded)
+            .borders(Borders::ALL)
+            .border_style(Style::default().fg(Color::White))
+            .style(Style::default())
+            .title("Options"),
+        general_layout[1],
+    );
+    frame.render_widget(
+        Block::new()
+            .border_type(BorderType::Rounded)
+            .borders(Borders::ALL)
+            .border_style(Style::default().fg(Color::White))
+            .style(Style::default())
+            .title("Communication"),
+        general_layout[2],
+    );
+
+    for midi in midi_layout {
+        frame.render_widget(
+            Block::new()
+                .border_type(BorderType::Rounded)
+                .borders(Borders::ALL)
+                .border_style(Style::default().fg(Color::White))
+                .style(Style::default())
+                .title("Midi"),
+            midi,
+        );
+    }
 }
