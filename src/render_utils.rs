7897c80e10bd6d3b91b07fa43b4ef00e3b3266df
Severin Sprenger <severin.sprenger@sespra.ch>
2025-05-09 10:37:09 +0200

    Added comments to renderers

--- src/render_utils.rs
+++ src/render_utils.rs
@@ -1,4 +1,10 @@
-use ratatui::{layout::{Constraint, Rect}, style::{Color, Style, Stylize}, symbols, widgets::{Axis, Block, BorderType, Borders, Chart, Dataset, GraphType, Padding, Row, Table}, Frame};
+use ratatui::{
+    Frame,
+    layout::{Constraint, Rect},
+    style::{Color, Style, Stylize},
+    symbols,
+    widgets::{Axis, Block, BorderType, Borders, Chart, Dataset, GraphType, Padding, Row, Table},
+};
 
 pub fn render_general(frame: &mut Frame, layout: Vec<Rect>) {
     frame.render_widget(
@@ -31,58 +37,67 @@ pub fn render_general(frame: &mut Frame, layout: Vec<Rect>) {
 }
 
 pub fn render_dds(frame: &mut Frame, layout: Rect, channel_data: &Vec<(f64, f64)>) {
+    // Create the dataset for the fft graph
     let dds_dataset = Dataset::default()
-            .marker(symbols::Marker::Braille)
-            .style(Style::new().fg(Color::Blue))
-            .graph_type(GraphType::Bar)
-            .data(channel_data);
+        .marker(symbols::Marker::Braille)
+        .style(Style::new().fg(Color::Blue))
+        .graph_type(GraphType::Bar)
+        .data(channel_data);
 
-        let chart = Chart::new(vec![dds_dataset.clone()])
-            .block(
-                Block::new()
-                    .borders(Borders::ALL)
-                    .border_type(BorderType::Thick)
-                    .title("FFT")
-                    .padding(Padding::new(1, 4, 1, 1)),
-            )
-            .x_axis(
-                Axis::default()
-                    .title("Frequency")
-                    .style(Style::default().fg(Color::White))
-                    .bounds([0., 6000.])
-                    .labels(["0", "3000", "6000"]),
-            )
-            .y_axis(
-                Axis::default()
-                    .title("Strength")
-                    .style(Style::default().fg(Color::White))
-                    .bounds([0., 10.])
-                    .labels(["0", "10"]),
-            );
+    // Create fft widget and the block surrounding it
+    let chart = Chart::new(vec![dds_dataset.clone()])
+        .block(
+            Block::new()
+                .borders(Borders::ALL)
+                .border_type(BorderType::Thick)
+                .title("FFT")
+                .padding(Padding::new(1, 4, 1, 1)),
+        )
+        .x_axis(
+            Axis::default()
+                .title("Frequency")
+                .style(Style::default().fg(Color::White))
+                .bounds([0., 6000.])
+                .labels(["0", "3000", "6000"]),
+        )
+        .y_axis(
+            Axis::default()
+                .title("Strength")
+                .style(Style::default().fg(Color::White))
+                .bounds([0., 10.])
+                .labels(["0", "10"]),
+        );
 
-        frame.render_widget(chart, layout);
+    frame.render_widget(chart, layout);
 }
 
 pub fn render_channels(frame: &mut Frame, layout: Vec<Rect>, channel_data: &Vec<(f64, f64)>) {
     for i in 0..layout.len() {
+        // Set default values used if a channel is unassigned
         let mut signal_freq = 0.;
-        let mut signal_strength = 0.;
+        let mut signal_strength = 10.;
 
+        // Check if a channel has valid data in it
         if i < channel_data.len() {
             signal_freq = channel_data[i].0;
             signal_strength = channel_data[i].1;
         }
 
+        // Convert numerical values to string
         let freq_str = &format!("{:.1} Hz", signal_freq);
         let attenu_str = &format!("{:.1}", 10. - signal_strength);
-
+        
+        // Create data rows
         let rows = [
             Row::new(vec!["Note", "IDFK"]),
             Row::new(vec!["Freq", freq_str]),
             Row::new(vec!["Atten", attenu_str]),
         ];
 
+        // Define how wide cells of table are
         let widths = [Constraint::Percentage(30), Constraint::Percentage(70)];
+
+        // Create table and the block surrounding it
         let table = Table::new(rows, widths)
             .column_spacing(1)
             .style(Style::new().white())
