53c01b0f8a69b569d1b001853c8757005f94106d
Severin Sprenger <severin.sprenger@sespra.ch>
2025-05-20 13:28:53 +0200

    Fixed integration issues

--- src/dds_data.rs
+++ src/dds_data.rs
@@ -16,9 +16,9 @@ impl DdsData {
 
         if self.signal_data.len() < 10 && !self.signal_data.contains(&(freq, signal_strength)) {
             com_config.send_midi(
-                    true as u8,
+                    0x90,
                     midi_utils::freq_to_note_id(freq),
-                    attenu as u8,
+                    (signal_strength * 25.5) as u8,
                 );
 
             self.signal_data.push((freq, signal_strength));
@@ -29,9 +29,9 @@ impl DdsData {
         for i in 0..self.signal_data.len() {
             if self.signal_data[i].0 == freq {
                 com_config.send_midi(
-                    false as u8,
+                    0x80,
                     midi_utils::freq_to_note_id(freq),
-                    (10.0 - self.signal_data[i].1) as u8,
+                    0x00,
                 );
 
                 self.signal_data.remove(i);
